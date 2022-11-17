41f1dbffcc4af1e506636fdeff44894089612c66
osouza <mauricio.souza002@gmail.com>
2022-11-17 19:14:27 -0200

    Inserir comentário descrevendo a função cem_anos_em

--- 1_Exercicio/1_Exercicio.py
+++ 1_Exercicio/1_Exercicio.py
@@ -4,12 +4,17 @@ def printar_pausar(message):
     print(message)
     time.sleep(2)
 
+# Esta função coleta a idade do usuário e retorna o ano em que o mesmo fará 100 anos.
+# Se o usuário inserir uma idade maior ou igual a 100, o programa printará solicitará
+# que o mesmo digite um número válido. Após a exibição do resultado, há a opção de 
+# inserir uma nova idade ou encerrar o programa.
 def cem_anos_em():
     ano_corrente = 2022
     idade_usuario = input("Qual é a sua idade?\n")
     while True:
         if int(idade_usuario) >= 100:
-            printar_pausar("Você já tem 100 anos ou mais. Por favor, \ndigite um número válido.")
+            printar_pausar("Você já tem 100 anos ou mais. Por favor, "
+                           "digite um número válido.")
             cem_anos_em()
         else:
             subtracao_anos = 100 - int(idade_usuario)
