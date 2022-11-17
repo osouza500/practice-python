a000851d25c430c9a5711ebcd6ad90d39d505d1a
osouza <mauricio.souza002@gmail.com>
2022-11-17 19:27:09 -0200

    Importar módulo datetime.

--- 1_Exercicio/1_Exercicio.py
+++ 1_Exercicio/1_Exercicio.py
@@ -1,33 +1,31 @@
-import time
-
-def printar_pausar(message):
-    print(message)
-    time.sleep(2)
+import datetime
 
 # Esta função coleta a idade do usuário e retorna o ano em que o mesmo fará 100 anos.
 # Se o usuário inserir uma idade maior ou igual a 100, o programa printará solicitará
 # que o mesmo digite um número válido. Após a exibição do resultado, há a opção de 
 # inserir uma nova idade ou encerrar o programa.
 def cem_anos_em():
-    ano_corrente = 2022
+    data_completa = datetime.date.today()
+    ano_corrente = data_completa.year
+    nome_usuario = input("Qual é o seu nome?\n")
     idade_usuario = input("Qual é a sua idade?\n")
     while True:
         if int(idade_usuario) >= 100:
-            printar_pausar("Você já tem 100 anos ou mais. Por favor, "
+            print("Você já tem 100 anos ou mais. Por favor, "
                            "digite um número válido.")
             cem_anos_em()
         else:
             subtracao_anos = 100 - int(idade_usuario)
-            printar_pausar(f"Você fará 100 anos em {ano_corrente + subtracao_anos}.")
+            print(f"{nome_usuario}, você fará 100 anos em {ano_corrente + subtracao_anos}.")
             while True:
-                printar_pausar("Quer digitar uma nova idade?")
+                print("Quer digitar uma nova idade?")
                 resposta = input("S/N\n").upper()
                 if resposta == "S":
                     cem_anos_em()
                 elif resposta == "N":
-                    printar_pausar("Até logo!")
+                    print("Até logo!")
                     quit()
                 else:
-                    printar_pausar("Por favor, digite uma resposta válida.")
+                    print("Por favor, digite uma resposta válida.")
             
 cem_anos_em()
