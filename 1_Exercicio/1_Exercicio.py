b17ec2b3433d002bfe3bfa3d74aed314196b40eb
osouza <mauricio.souza002@gmail.com>
2022-11-17 19:02:01 -0200

    Inserir loops while na função cem_anos_em

--- 1_Exercicio/1_Exercicio.py
+++ 1_Exercicio/1_Exercicio.py
@@ -1,15 +1,30 @@
 import time
 
-def print_pause(message):
+def printar_pausar(message):
     print(message)
     time.sleep(1)
 
 def cem_anos_em():
     ano_corrente = 2022
-    idade_usuario = print_pause(input("Qual é a sua idade?\n"))
-    if idade_usuario > 100:
-        print_pause("Por favor, digite um número válido.")
-    else:
-        subtracao_anos = 100 - int(idade_usuario)
-        resultado = ano_corrente + subtracao_anos
-        print_pause(f"Você fará 100 anos em {resultado }.")
+    idade_usuario = input("Qual é a sua idade?\n")
+    while True:
+        if int(idade_usuario) >= 100:
+            print("Você já tem 100 anos ou mais. Por favor, \ndigite um número válido.")
+            cem_anos_em()
+        else:
+            subtracao_anos = 100 - int(idade_usuario)
+            printar_pausar(f"Você fará 100 anos em {ano_corrente + subtracao_anos}.")
+            while True:
+                printar_pausar("Quer digitar uma nova idade?")
+                resposta = input("S/N\n").lower()
+                if resposta == "s":
+                    cem_anos_em()
+                elif resposta == "n":
+                    printar_pausar("Até logo!")
+                    break
+                else:
+                    printar_pausar("Por favor, digite uma resposta válida.")
+            
+            
+
+cem_anos_em()
