3eedbdf640393aba949e703b7461d382f6b1fb19
osouza <mauricio.souza002@gmail.com>
2022-11-17 18:38:36 -0200

    Criar função print_pause; inserir programa na função cem_anos_em

--- 1_Exercicio/1_Exercicio.py
+++ 1_Exercicio/1_Exercicio.py
@@ -1,7 +1,15 @@
 import time
 
-ano_corrente = 2022
-idade_usuario = input("Qual é a sua idade?\n")
-subtracao_anos = int(idade_usuario) - 100
+def print_pause(message):
+    print(message)
+    time.sleep(1)
 
-time.sleep(1)
+def cem_anos_em():
+    ano_corrente = 2022
+    idade_usuario = print_pause(input("Qual é a sua idade?\n"))
+    if idade_usuario > 100:
+        print_pause("Por favor, digite um número válido.")
+    else:
+        subtracao_anos = 100 - int(idade_usuario)
+        resultado = ano_corrente + subtracao_anos
+        print_pause(f"Você fará 100 anos em {resultado }.")
