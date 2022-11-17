4206c7014ad783ea0ab63f9c4bf89f72bc6f45a7
osouza <mauricio.souza002@gmail.com>
2022-11-17 19:07:11 -0200

    Corrigir loop while infinito dentro da função cem_anos_em

--- 1_Exercicio/1_Exercicio.py
+++ 1_Exercicio/1_Exercicio.py
@@ -2,29 +2,27 @@ import time
 
 def printar_pausar(message):
     print(message)
-    time.sleep(1)
+    time.sleep(2)
 
 def cem_anos_em():
     ano_corrente = 2022
     idade_usuario = input("Qual é a sua idade?\n")
     while True:
         if int(idade_usuario) >= 100:
-            print("Você já tem 100 anos ou mais. Por favor, \ndigite um número válido.")
+            printar_pausar("Você já tem 100 anos ou mais. Por favor, \ndigite um número válido.")
             cem_anos_em()
         else:
             subtracao_anos = 100 - int(idade_usuario)
             printar_pausar(f"Você fará 100 anos em {ano_corrente + subtracao_anos}.")
             while True:
                 printar_pausar("Quer digitar uma nova idade?")
-                resposta = input("S/N\n").lower()
-                if resposta == "s":
+                resposta = input("S/N\n").upper()
+                if resposta == "S":
                     cem_anos_em()
-                elif resposta == "n":
+                elif resposta == "N":
                     printar_pausar("Até logo!")
-                    break
+                    quit()
                 else:
                     printar_pausar("Por favor, digite uma resposta válida.")
             
-            
-
 cem_anos_em()
