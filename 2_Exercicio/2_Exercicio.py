7bef8896e92868d4c948ad15daf9f00e7e9f6de5
osouza <mauricio.souza002@gmail.com>
2022-11-18 19:27:55 -0200

    Inserir novo input e instrução if

--- 2_Exercicio/2_Exercicio.py
+++ 2_Exercicio/2_Exercicio.py
@@ -1,10 +1,23 @@
 def par_ou_impar():
-    numero = int(input("Por favor, digite um número\n"))
+    numero = int(input("Por favor, digite um número.\n"))
     if numero % 4 == 0 and numero % 2 == 0:
         print(f"{numero} é um número par e múltiplo de 4.")
     elif numero % 4 != 0 and numero % 2 == 0:
         print(f"{numero} é um número par.")
     elif numero % 2 != 0:
         print(f"{numero} é um número ímpar.")
+
+    # Desafio extra: coletar dois números e verificar se a
+    # divisão entre ambos é exata.
+    numero_2 = int(input("Por favor, digite outro número.\n"))
+    checar_numero = int(input("Agora digite outro número para "
+                              "verificar se a divisão entre " 
+                              "ambos é exata.\n"))
+    resultado = numero_2 % checar_numero
+    if resultado == 0:
+        print(f"{numero_2} dividido por {checar_numero} é "
+               "uma divisão exata.")
+    elif resultado =! 0: 
+
 
 par_ou_impar()
