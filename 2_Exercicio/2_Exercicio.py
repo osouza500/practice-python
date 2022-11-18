084120cc1fc646277b464ce13b429dd4ecee7ac3
osouza <mauricio.souza002@gmail.com>
2022-11-18 19:08:00 -0200

    Inserir declaração condicional, verificar múltiplo de 4

--- 2_Exercicio/2_Exercicio.py
+++ 2_Exercicio/2_Exercicio.py
@@ -1,8 +1,10 @@
 def par_ou_impar():
     numero = int(input("Por favor, digite um número\n"))
-    if numero % 2 == 0:
-        print(f"{numero} é um número par")
+    if numero % 4 == 0 and numero % 2 == 0:
+        print(f"{numero} é um número par e múltiplo de 4.")
+    elif numero % 4 != 0 and numero % 2 == 0:
+        print(f"{numero} é um número par.")
     elif numero % 2 != 0:
-        print(f"{numero} é um número ímpar")
+        print(f"{numero} é um número ímpar.")
 
 par_ou_impar()
