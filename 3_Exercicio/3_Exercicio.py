760010bbcd5485ed818ffc06ddcc9ae78914478b
osouza <mauricio.souza002@gmail.com>
2022-11-21 18:04:38 -0200

    Importar módulo time; criar função printar_pausar

--- 3_Exercicio/3_Exercicio.py
+++ 3_Exercicio/3_Exercicio.py
@@ -1,4 +1,12 @@
+import time
+
+def printar_pausar(mensagem):
+    time.sleep(1)
+    print(mensagem)
+
 lista = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+nova_lista = []
 for numero in lista:
     if numero < 5:
-        print(numero)
+        nova_lista.append(numero)
+print(nova_lista)
