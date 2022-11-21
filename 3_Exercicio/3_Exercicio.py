99c61d13fa636f1f7d8fee36505b87f3bb5ccb3c
osouza <mauricio.souza002@gmail.com>
2022-11-21 19:01:04 -0200

    Criar função continue_quit

--- 3_Exercicio/3_Exercicio.py
+++ 3_Exercicio/3_Exercicio.py
@@ -4,13 +4,25 @@ def printar_pausar(mensagem):
     time.sleep(1)
     print(mensagem)
 
-lista = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+def continue_quit():
+    while True:
+        printar_pausar("Quer digitar outro número?")
+        printar_pausar("S/N")
+        opcao_usuario = input()
+        if opcao_usuario == "S":
+            menor_que()
+        elif opcao_usuario == "N":
+            printar_pausar("Até logo!")
+            quit()
+        else:
+            printar_pausar("Por favor, digite uma resposta válida.")
+            continue_quit()   
 
-numero_usuario = int(input("Por favor, digite um número.\n"))
-print([numeros for numeros in lista if numero_usuario < numeros])
+def menor_que():
+    lista = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    printar_pausar("Por favor, digite um número.")
+    numero_usuario = int(input())
+    printar_pausar([numeros for numeros in lista if numero_usuario > numeros])
+    continue_quit()
 
-
-# for numero in lista:
-#     if numero < 5:
-#         nova_lista.append(numero)
-# print(nova_lista)
+menor_que()
