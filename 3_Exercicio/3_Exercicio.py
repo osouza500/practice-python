bddd4477fbdfd0e5bcaf70d686873ed266da9e94
osouza <mauricio.souza002@gmail.com>
2022-11-21 19:16:08 -0200

    Inserir script footer; formatar código de acordo com pycodestyle

--- 3_Exercicio/3_Exercicio.py
+++ 3_Exercicio/3_Exercicio.py
@@ -1,31 +1,30 @@
 import time
 
-def printar_pausar(mensagem):
-    time.sleep(1)
-    print(mensagem)
 
 def continue_quit():
-    while True:
-        printar_pausar("Quer digitar outro número?")
-        resposta = input("S/N\n").upper()
-        if resposta == "S":
-            menor_que()
-        elif resposta == "N":
-            printar_pausar("Até logo!")
-            quit()
-        else:
-            printar_pausar("Por favor, digite uma resposta válida.")
-            continue_quit()   
+    print("Quer digitar outro número?")
+    resposta = input("S/N\n").upper()
+    if resposta == "S":
+        menor_que()
+    elif resposta == "N":
+        print("Até logo!")
+        quit()
+    else:
+        print("Por favor, digite uma resposta válida.")
+        continue_quit()
+
 
 def menor_que():
     try:
         lista = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-        printar_pausar("Por favor, digite um número.")
+        print("Por favor, digite um número.")
         numero_usuario = int(input())
-        printar_pausar([numeros for numeros in lista if numero_usuario > numeros])
+        print([numeros for numeros in lista if numero_usuario > numeros])
         continue_quit()
     except ValueError:
-        printar_pausar("Por favor, digite um número válido.")
-        menor_que()    
+        print("Por favor, digite um número válido.")
+        menor_que()
 
-menor_que()
+
+if __name__ == "__main__":
+    menor_que()
