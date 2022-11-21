64bb09db17f5ddc62f6289ec4639980a8693f6fa
osouza <mauricio.souza002@gmail.com>
2022-11-21 19:05:21 -0200

    Inserir instrução try/except na função menor_que

--- 3_Exercicio/3_Exercicio.py
+++ 3_Exercicio/3_Exercicio.py
@@ -7,11 +7,10 @@ def printar_pausar(mensagem):
 def continue_quit():
     while True:
         printar_pausar("Quer digitar outro número?")
-        printar_pausar("S/N")
-        opcao_usuario = input()
-        if opcao_usuario == "S":
+        resposta = input("S/N\n").upper()
+        if resposta == "S":
             menor_que()
-        elif opcao_usuario == "N":
+        elif resposta == "N":
             printar_pausar("Até logo!")
             quit()
         else:
@@ -19,10 +18,14 @@ def continue_quit():
             continue_quit()   
 
 def menor_que():
-    lista = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-    printar_pausar("Por favor, digite um número.")
-    numero_usuario = int(input())
-    printar_pausar([numeros for numeros in lista if numero_usuario > numeros])
-    continue_quit()
+    try:
+        lista = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+        printar_pausar("Por favor, digite um número.")
+        numero_usuario = int(input())
+        printar_pausar([numeros for numeros in lista if numero_usuario > numeros])
+        continue_quit()
+    except ValueError:
+        printar_pausar("Por favor, digite um número válido.")
+        menor_que()    
 
 menor_que()
