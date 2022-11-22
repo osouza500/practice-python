b18c8ae5e9cd11cf3029d79da3470c001855df40
osouza <mauricio.souza002@gmail.com>
2022-11-22 18:45:29 -0200

    InsInserir função input; inserir loop for

---
+++ 4_Exercicio/4_Exercicio.py
@@ -0,0 +1,6 @@
+usuario_numero = print(int(input("Por favor, digite um número.")))
+numeros_lista = range(1, 101)
+resultado_lista = []
+for numeros in numeros_lista:
+    if usuario_numero % numeros == 0:
+        numeros.append(resultado_lista)
