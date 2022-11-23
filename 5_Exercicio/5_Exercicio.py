688b7e7b6172bd560a58b23dd006ee9a662242fb
osouza <mauricio.souza002@gmail.com>
2022-11-23 18:15:00 -0200

    Remover e reescrever função comparar_listas

--- 5_Exercicio/5_Exercicio.py
+++ 5_Exercicio/5_Exercicio.py
@@ -3,7 +3,7 @@ import random
 def lista_aleatoria():
     lista = []
     for count in range(random.randint(1, 31)):
-        numero = random.randint(1, 100)
+        numero = random.randint(1, 10)
         if numero not in lista:
             lista.append(numero)
         else:
@@ -11,15 +11,7 @@ def lista_aleatoria():
     return lista
 
 def comparar_listas():
-    lista_1 = lista_aleatoria()
-    lista_2 = lista_aleatoria()
-    lista_final = []
-    contagem = 0
-    for numero in range(1, 100):
-        if lista_1[contagem] == lista_2[contagem]:
-            
-            contagem += 1
-    print(lista_final)        
+    
 
 comparar_listas()        
 
