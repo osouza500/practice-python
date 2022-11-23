7f8045acd90c1625278bf07f996667b5d80f6216
osouza <mauricio.souza002@gmail.com>
2022-11-23 17:03:15 -0200

    Importar módulo random; inserir loop for

---
+++ 5_Exercicio/5_Exercicio.py
@@ -0,0 +1,20 @@
+import random 
+
+list_1 = []
+list_2 = []
+
+for contagem in range(1, 31):
+    numero = random.randint(1, 101)
+    list_1.append(numero)
+    
+
+
+
+import random
+
+randomlist = []
+
+for i in range(0,5):
+    n = random.randint(1,30)
+    randomlist.append(n)
+    print(randomlist)
