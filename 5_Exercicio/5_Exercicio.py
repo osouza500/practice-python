b87584a1af7de48f74dfc48296047f390ff7b29d
osouza <mauricio.souza002@gmail.com>
2022-11-23 19:07:54 -0200

    Inserir função len() para comparar listas

--- 5_Exercicio/5_Exercicio.py
+++ 5_Exercicio/5_Exercicio.py
@@ -2,8 +2,8 @@ import random
 
 def lista_aleatoria():
     lista = []
-    for count in range(random.randint(1, 31)):
-        numero = random.randint(1, 10)
+    for count in range(random.randint(1, 30)):
+        numero = random.randint(1, 30)
         if numero not in lista:
             lista.append(numero)
         else:
@@ -11,7 +11,20 @@ def lista_aleatoria():
     return lista
 
 def comparar_listas():
+    lista_1 = lista_aleatoria()
+    lista_2 = lista_aleatoria()
+    lista_final = []
+    contagem = 0
+    if len(lista_1) > len(lista_2):
+        
+
+    
+      
+
+    # print(lista_1)
+    # print(lista_2)    
+    # print(len(lista_1))
+    # print(len(lista_2))
     
 
 comparar_listas()        
-
