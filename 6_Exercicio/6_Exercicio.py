11b3f9ed759ff1c9b07709da4dd6c7b2ab3d9a87
osouza <mauricio.souza002@gmail.com>
2022-11-29 18:13:50 -0200

    Inserir instrução condicional

--- 6_Exercicio/6_Exercicio.py
+++ 6_Exercicio/6_Exercicio.py
@@ -1,10 +1,15 @@
 def e_palindromo():
     print("Escreva uma palavra ou frase.")
     resposta = input().lower()
-    indice = 0
-    for count in range(len(resposta)):
-        print(resposta[indice])
-        indice += 1
-
+    ao_contrario = []
+    indice = -1
+    for letra in resposta:
+        ao_contrario.append(resposta[indice])
+        indice -= 1
+    ao_contrario = "".join(ao_contrario)
+    if ao_contrario == resposta:
+        print(f"A palavra/frase {resposta} é um palíndromo.")
+    else:
+        print(f"A palavra {resposta} não é um palíndromo.")      
 
 e_palindromo()
