27bcf4ee7c30aeacc95fc922e1e496e727d565ff
osouza <mauricio.souza002@gmail.com>
2022-11-29 18:38:41 -0200

    Inserir script footer

--- 6_Exercicio/6_Exercicio.py
+++ 6_Exercicio/6_Exercicio.py
@@ -1,15 +1,17 @@
 def e_palindromo():
     print("Escreva uma palavra ou frase.")
-    resposta = input().lower()
+    resposta_original = input()
+    comparar_resposta = resposta_original.lower().replace(" ", "")
     ao_contrario = []
     indice = -1
-    for letra in resposta:
-        ao_contrario.append(resposta[indice])
+    for letra in comparar_resposta:
+        ao_contrario.append(comparar_resposta[indice])
         indice -= 1
     ao_contrario = "".join(ao_contrario)
-    if ao_contrario == resposta:
-        print(f"A palavra/frase {resposta} é um palíndromo.")
+    if ao_contrario == comparar_resposta:
+        print(f"A palavra/frase {resposta_original} é um palíndromo.")
     else:
-        print(f"A palavra {resposta} não é um palíndromo.")      
+        print(f"A palavra/frase {resposta_original} não é um palíndromo.")      
 
-e_palindromo()
+if __name__ == "__main__":
+    e_palindromo()
