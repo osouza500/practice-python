8ac3a7ac3bd53bf27b71bc6249f85b7a0419947e
osouza <mauricio.souza002@gmail.com>
2022-12-02 19:07:10 -0200

    Inserir opção de continuar/sair

--- 8_Exercicio/8_Exercicio.py
+++ 8_Exercicio/8_Exercicio.py
@@ -8,25 +8,29 @@ def condicoes_vitoria(primeira_jogada, segunda_jogada):
 
 def jogo():
     jogadas = ["pedra", "papel", "tesoura"]
-    jogada_humano = input("Pedra, papel ou tesoura?\n").lower()
-    jogada_computador = random.choice(jogadas)
-    if jogada_humano == jogada_computador:
-        print(f"Humano: {jogada_humano}; Computador: {jogada_computador}")
-        print("Empate!\n")
-        jogo()
-    if condicoes_vitoria(jogada_humano, jogada_computador):
-        print(f"Humano: {jogada_humano}; Computador: {jogada_computador}")
-        print("Humano vence máquina!\n")
-    elif condicoes_vitoria(jogada_computador, jogada_humano):
-        print(f"Humano: {jogada_humano}; Computador: {jogada_computador} ")
-        print("Computador vence humano!\n")
     while True:
-        continuar_sair = input("Jogar novamente? S/N\n").lower()
-        if continuar_sair == "s":
-            jogo()
-        elif continuar_sair == "n":
-            quit()
-        else:
+        jogada_humano = input("Pedra, papel ou tesoura?\n").lower()
+        jogada_computador = random.choice(jogadas)
+        if jogada_humano not in jogadas:
             print("Digite um input válido.\n")
+        else:
+            if jogada_humano == jogada_computador:
+                print(f"Humano: {jogada_humano}; Computador: {jogada_computador}")
+                print("Empate!\n")
+                jogo()
+            if condicoes_vitoria(jogada_humano, jogada_computador):
+                print(f"Humano: {jogada_humano}; Computador: {jogada_computador}")
+                print("Humano vence máquina!\n")
+            elif condicoes_vitoria(jogada_computador, jogada_humano):
+                print(f"Humano: {jogada_humano}; Computador: {jogada_computador} ")
+                print("Computador vence humano!\n")
+            while True:
+                continuar_sair = input("Jogar novamente? S/N\n").lower()
+                if continuar_sair == "s":
+                    jogo()
+                elif continuar_sair == "n":
+                    quit()
+                else:
+                    print("Digite um input válido.\n")
 
 jogo()
