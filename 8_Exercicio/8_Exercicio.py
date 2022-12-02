9b3319176fce6f9452d1062f6d8a1101166597fc
osouza <mauricio.souza002@gmail.com>
2022-12-02 19:02:40 -0200

    Inserir quebras de linha

--- 8_Exercicio/8_Exercicio.py
+++ 8_Exercicio/8_Exercicio.py
@@ -8,8 +8,25 @@ def condicoes_vitoria(primeira_jogada, segunda_jogada):
 
 def jogo():
     jogadas = ["pedra", "papel", "tesoura"]
-    jogada_humano = input("Pedra, papel ou tesour?\n").lower()
+    jogada_humano = input("Pedra, papel ou tesoura?\n").lower()
     jogada_computador = random.choice(jogadas)
     if jogada_humano == jogada_computador:
-        print("Empate!")
+        print(f"Humano: {jogada_humano}; Computador: {jogada_computador}")
+        print("Empate!\n")
         jogo()
+    if condicoes_vitoria(jogada_humano, jogada_computador):
+        print(f"Humano: {jogada_humano}; Computador: {jogada_computador}")
+        print("Humano vence máquina!\n")
+    elif condicoes_vitoria(jogada_computador, jogada_humano):
+        print(f"Humano: {jogada_humano}; Computador: {jogada_computador} ")
+        print("Computador vence humano!\n")
+    while True:
+        continuar_sair = input("Jogar novamente? S/N\n").lower()
+        if continuar_sair == "s":
+            jogo()
+        elif continuar_sair == "n":
+            quit()
+        else:
+            print("Digite um input válido.\n")
+
+jogo()
