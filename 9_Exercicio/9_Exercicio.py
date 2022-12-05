d5eb8ad911452b9c8da2d995370a5570380c8b15
osouza <mauricio.souza002@gmail.com>
2022-12-05 20:43:45 -0200

    Criar função continuar_sair

--- 9_Exercicio/9_Exercicio.py
+++ 9_Exercicio/9_Exercicio.py
@@ -1,18 +1,38 @@
 import random
 
+def continuar_sair():
+    resposta = input("Desistir? S/N\n").lower()
+    if resposta == "s":
+         print("Até logo!")
+         quit()
+    elif resposta == "n":
+        pass
+    else:
+        print("Input inválido.")
+        continuar_sair()
+
 def jogo_adivinhacao ():
     numero = random.randint(1, 9)
-    print("Bem-vindo ao jogo de adivinhação.\n")
-    resposta_usuario = int(input("Digite um número de 1 a 9.\n"))
-    if resposta_usuario < 1 or resposta_usuario > 9:
-        print("Digite um número válido.\n")
-        jogo_adivinhacao()
-    elif resposta_usuario == numero:
-        print("Na mosca!")
-    elif resposta_usuario < numero:
-        print("Chutou baixo.")
-    elif resposta_usuario > numero:
-        print("Chutou alto.")
+    while True:
+        try:
+            resposta_usuario = int(input("Digite um número de 1 a 9.\n"))
+        except ValueError:
+            print("Input inválido\n")
+            jogo_adivinhacao()
+        if resposta_usuario < 1 or resposta_usuario > 9:
+            print("Input inválido.\n")
+            jogo_adivinhacao()
+        elif resposta_usuario == numero:
+            print("Na mosca!")
+            quit()
+        elif resposta_usuario < numero:
+            print("Chutou baixo.")
+            continuar_sair()
+        elif resposta_usuario > numero:
+            print("Chutou alto.")
+            continuar_sair()
+    
+
 
 jogo_adivinhacao()
     
