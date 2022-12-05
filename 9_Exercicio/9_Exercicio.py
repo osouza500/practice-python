235fc806b9a6a39137b9463deac81f2472495382
osouza <mauricio.souza002@gmail.com>
2022-12-05 21:07:55 -0200

    Recriar função continuar_sair

--- 9_Exercicio/9_Exercicio.py
+++ 9_Exercicio/9_Exercicio.py
@@ -1,36 +1,40 @@
 import random
 
+def continuar_sair ():
+    resposta = input("Jogar novamente? S/N.").lower()
+    if resposta == "s":
+        jogo_adivinhacao()
+    elif resposta == "n":
+        print("Até logo!")
+        quit()
+    else:
+        print("Input inválido.")
+        continuar_sair()
+
 def jogo_adivinhacao ():
     tentativas = 0
     numero = random.randint(1, 9)
     while True:
         try:
-            resposta_usuario = int(input("Digite um número de 1 a 9 "
-                                         "ou SAIR para sair.\n"))
+            resposta_usuario = int(input("Digite um número de 1 a 9.\n"))
         except ValueError:
-            if resposta_usuario == "SAIR":
-                print("Até logo!")
-                quit()
-            else:
-                print("Input inválido.")
-                jogo_adivinhacao()
-        if int(resposta_usuario) < 1 or int(resposta_usuario) > 9:
+            print("Input inválido.\n")
+            jogo_adivinhacao()
+        if resposta_usuario < 1 or resposta_usuario > 9:
             print("Input inválido.\n")
             jogo_adivinhacao()
         elif resposta_usuario == numero:
             tentativas += 1
             print("Na mosca!")
-            print(f"Você tentou {tentativas} vezes.")
-            print("Até logo!")
-            quit()
+            print(f"O número é {numero}! Você tentou "
+                  f"{tentativas} vezes.")
+            continuar_sair()
         elif resposta_usuario < numero:
             tentativas += 1
             print("Chutou baixo.")
-            continuar_sair()
         elif resposta_usuario > numero:
             tentativas += 1
             print("Chutou alto.")
-            continuar_sair()
     
 jogo_adivinhacao()
     
