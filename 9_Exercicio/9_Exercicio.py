34f33053fd83dcf8b70dc7edfbdcefc09c500d8d
osouza <mauricio.souza002@gmail.com>
2022-12-05 20:12:56 -0200

    Importar módulo random; criar função jogo_adivinhacao

---
+++ 9_Exercicio/9_Exercicio.py
@@ -0,0 +1,7 @@
+import random
+
+def jogo_adivinhacao ():
+    numero = random.randint(1, 9)
+    resposta_usuario = input("Digite um número de 1 a 9.")
+    if resposta_usuario != int or resposta_usuario <
+    
