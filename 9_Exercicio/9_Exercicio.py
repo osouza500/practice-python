bc2141f3b51d900cc2ac8212c1c42e32d9f4f243
osouza <mauricio.souza002@gmail.com>
2022-12-05 20:20:40 -0200

    Inserir instruções condicionais

--- 9_Exercicio/9_Exercicio.py
+++ 9_Exercicio/9_Exercicio.py
@@ -2,6 +2,17 @@ import random
 
 def jogo_adivinhacao ():
     numero = random.randint(1, 9)
-    resposta_usuario = input("Digite um número de 1 a 9.")
-    if resposta_usuario != int or resposta_usuario <
+    print("Bem-vindo ao jogo de adivinhação.\n")
+    resposta_usuario = int(input("Digite um número de 1 a 9.\n"))
+    if resposta_usuario < 1 or resposta_usuario > 9:
+        print("Digite um número válido.\n")
+        jogo_adivinhacao()
+    elif resposta_usuario == numero:
+        print("Na mosca!")
+    elif resposta_usuario < numero:
+        print("Chutou baixo.")
+    elif resposta_usuario > numero:
+        print("Chutou alto.")
+
+jogo_adivinhacao()
     
