710c633f4b9383b0e85b4d17bf9e50711a828a84
osouza <mauricio.souza002@gmail.com>
2022-12-12 18:32:59 -0200

    Inserir instrução try/except na função input_usuario

--- 11_Exercicio/11_Exercicio.py
+++ 11_Exercicio/11_Exercicio.py
@@ -1,12 +1,23 @@
 def input_usuario():
-    numero = int(input("Digite um número.\n"))
-    return numero
+    try:
+        numero = int(input("Digite um número maior que 1.\n"))
+        if numero <= 1:
+            print("Input inválido.")
+            input_usuario()
+        else:
+            return numero
+    except ValueError:
+        print("Input inválido.")
+        input_usuario()
 
 def numero_primo():
     numero = input_usuario()
-    for ciclo in range(numero):
+    for ciclo in range(2, numero):
         if numero % ciclo == 0:
             print(f"{numero} não é um número primo.")
+            break
         else:
             print(f"{numero} é um número primo.")
+            break
         
+numero_primo()
