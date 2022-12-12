17f380ca73605977903936a62c6c40e303a0de4f
osouza <mauricio.souza002@gmail.com>
2022-12-12 17:52:17 -0200

    Criar função numero_primo

--- 11_Exercicio/11_Exercicio.py
+++ 11_Exercicio/11_Exercicio.py
@@ -2,5 +2,11 @@ def input_usuario():
     numero = int(input("Digite um número.\n"))
     return numero
 
-
-def numero_primo(num):
+def numero_primo():
+    numero = input_usuario()
+    for ciclo in range(numero):
+        if numero % ciclo == 0:
+            print(f"{numero} não é um número primo.")
+        else:
+            print(f"{numero} é um número primo.")
+        
