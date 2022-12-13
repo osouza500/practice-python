0109e6272b1beabeb462802a483ce4c0a2e4eecc
osouza <mauricio.souza002@gmail.com>
2022-12-13 09:34:35 -0200

    Refatorar função número_primo

--- 11_Exercicio/11_Exercicio.py
+++ 11_Exercicio/11_Exercicio.py
@@ -2,7 +2,7 @@ def input_usuario():
     while True:
         try:
             resposta = int(input("Digite um número de 2 a 100.\n"))
-            if resposta <= 1 or resposta >= 100:
+            if resposta <= 1 or resposta > 100:
                 print("Input inválido.")
             else:
                 return resposta
@@ -12,12 +12,12 @@ def input_usuario():
 def numero_primo(numero):
     if numero == 2:
         print("2 é um número primo.")
+        quit()
     for ciclo in range(2, numero):
         if numero % ciclo == 0:
             print(f"{numero} não é um número primo.")
-            break
-        else:
-            print(f"{numero} é um número primo.")
-            break
+            quit()
+    print(f"{numero} é um número primo.")
         
+
 numero_primo(input_usuario())
