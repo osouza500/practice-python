b09a7355bb3b566924c896cd52d4157625c7fa19
osouza <mauricio.souza002@gmail.com>
2022-12-13 19:07:36 -0200

    Inserir compreensão de lista na função primeiro_ultimo

--- 12_Exercicio/12_Exercicio.py
+++ 12_Exercicio/12_Exercicio.py
@@ -1,3 +1,10 @@
 def primeiro_ultimo():
     lista = [5, 10, 15, 20, 25]
-    nova_lista = nova_lista.append(lista[0],[-1])
+    nova_lista = [
+        numero for numero in lista 
+        if numero == lista[0] 
+        or numero == lista[-1]
+        ]
+    print(nova_lista)
+
+primeiro_ultimo()
