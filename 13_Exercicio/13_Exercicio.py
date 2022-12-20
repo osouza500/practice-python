2bdbc45f944b35a24028d1dcacdee63f948cca1a
osouza <mauricio.souza002@gmail.com>
2022-12-20 18:27:11 -0200

    Criar função sequencia_fibonacci

--- 13_Exercicio/13_Exercicio.py
+++ 13_Exercicio/13_Exercicio.py
@@ -6,5 +6,17 @@ def printar_pausar(mensagem):
     print(mensagem)
 
 def sequencia_fibonacci():
-    printar_pausar("Digite o número correspondente à quantidade de números na sequência:")
-    qtdnum_sequencia = int(input())
+    seq_fib = []
+    printar_pausar("Quantos números na sequência?")
+    qtd_num = int(input())
+    for ciclo in range(qtd_num):
+        if 0 not in seq_fib:
+            seq_fib.append(0)
+        elif 1 not in seq_fib:
+            seq_fib.append(1)
+        else:
+            seq_fib.append(seq_fib[-1] + seq_fib[-2])
+    print(seq_fib)
+
+sequencia_fibonacci()
+
