8bc5945756745ca95f2952ba564d1d104cb783ef
osouza <mauricio.souza002@gmail.com>
2023-01-05 17:21:40 -0200

    Inserir script footer

--- 14_Exercicio/14_Exercicio.py
+++ 14_Exercicio/14_Exercicio.py
@@ -1,12 +1,17 @@
 import random
 
+
 def gerador_lista():
+    # cria uma lista aleatória
     lista_aleatoria = [random.randint(1, 50) for ciclo in range(random.randint(1, 50))]
     return lista_aleatoria
 
+
 def remover_repetidos(lista):
     print(f"Primeira lista: {lista}\n")
-    lista = list(set(lista))
-    print(f"Segunda lista (sem repetições): {lista}")
+    # remove números repetidos
+    print(f"Segunda lista (sem repetições): {list(set(lista))}")
 
-remover_repetidos(gerador_lista())
+
+if __name__ == "__main__":
+    remover_repetidos(gerador_lista())
