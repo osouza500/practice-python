a842ad668b470efbc76b8eeb3b9a37454d9ffcb8
osouza <mauricio.souza002@gmail.com>
2023-01-05 17:11:09 -0200

    Criar função remover_repetidos

--- 14_Exercicio/14_Exercicio.py
+++ 14_Exercicio/14_Exercicio.py
@@ -4,3 +4,9 @@ def gerador_lista():
     lista_aleatoria = [random.randint(1, 50) for ciclo in range(random.randint(1, 50))]
     return lista_aleatoria
 
+def remover_repetidos(lista):
+    print(f"Primeira lista: {lista}\n")
+    lista = list(set(lista))
+    print(f"Segunda lista (sem repetições): {lista}")
+
+remover_repetidos(gerador_lista())
