0b9f356f06018879c88cc6e493820dbadabf575a
osouza <mauricio.souza002@gmail.com>
2023-01-20 14:59:28 -0200

    Criar função frase_contrario

---
+++ 15_Exercicio/15_Exercicio.py
@@ -0,0 +1,13 @@
+
+def frase_contrario():
+    resposta = input("Digite uma frase.\n")
+    frase = resposta.split()
+    frase_contrario = []
+    for i in range(len(frase)):
+        
+        
+    print(frase)
+
+    
+frase_contrario()
+
